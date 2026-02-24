99bb25fd535f19019136e9a0d099a5a7097f88d6
Tlaxcala14 <oscarperales14@gmail.com>
2026-02-24 12:55:59 -0600

    hice base de datos

---
+++ base_de_datos.sql
@@ -0,0 +1,23 @@
+create database gestor;
+use database gestor;
+
+-- Tabla para almacenar los usuarios del sistema --
+create table usuarios (
+   id int auto_increment primary key,
+   usuario varchar(50) not null,
+   contra varchar (12) not null
+);
+
+-- Tabla para almacenar las tareas de los usuarios --
+create table tareas (
+   id_tarea int auto_increment primary key,
+   titulo varchar(50) not null,
+   descripcion text null,
+   prioridad int not null,       -- 1 baja, 2 media, 3 alta--
+   estado int not null,          -- 1 pendiente, 2 en progreso, 3 completada --
+   fecha_limite date not null,   -- YYYY-MM-DD --
+   usuario_id int not null,
+   foreign key (usuario_id) references usuarios(id)
+);
+
+INSERT INTO usuarios (usuario, contra) VALUES ('admin', 'Admin123.'); -- Usuario de prueba para acceder al sistema --
